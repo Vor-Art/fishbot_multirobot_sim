cb21c7a956ed00937247993f9f835322ce826dc8
Artem Voronov <a.voronov@innopolis.university>
2025-12-11 14:27:32 +0800

    feat: avoid docker rebuild and refacctor some code

--- src/esdf_map/include/esdf_map/esdf_map_core.hpp
+++ src/esdf_map/include/esdf_map/esdf_map_core.hpp
@@ -6,16 +6,13 @@
 
 namespace esdf_map
 {
-
-    struct Vec3
-    {
+    struct Vec3 {
         double x{0.0};
         double y{0.0};
         double z{0.0};
     };
 
-    struct Costmap2d
-    {
+    struct Costmap2d {
         double resolution{0.25};
         double size_m{20.0};
         uint32_t width{0};
@@ -23,22 +20,19 @@ namespace esdf_map
         std::vector<int8_t> data;
     };
 
-    struct QueryRequest
-    {
+    struct QueryRequest {
         Vec3 position;
         double max_range{0.0};
     };
 
-    struct QueryResponse
-    {
+    struct QueryResponse {
         double distance{0.0};
         Vec3 gradient;
         bool success{false};
         std::string message;
     };
 
-    class EsdfMapCore
-    {
+    class EsdfMapCore {
     public:
         EsdfMapCore();
         EsdfMapCore(double default_distance, double costmap_resolution, double costmap_size_m);
